092b8048242d9b93b4470801211ff16677ce8598
Maciej Iskra <s168723@student.uken.krakow.pl>
2026-01-02 20:06:02 +0100

    Implement authentication for write operations

--- app/routers/books.py
+++ app/routers/books.py
@@ -1,10 +1,28 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+import secrets
 
 from app import models, schemas
 from app.database import SessionLocal
 
 router = APIRouter(prefix="/books")
+security = HTTPBasic()
+
+
+def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
+    # Login: admin, Hasło: secret
+    correct_username = secrets.compare_digest(credentials.username, "admin")
+    correct_password = secrets.compare_digest(credentials.password, "secret")
+
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
+
 
 def get_db():
     db = SessionLocal()
@@ -19,14 +37,6 @@ def list_books(db: Session = Depends(get_db)):
     return db.query(models.Book).all()
 
 
-@router.post("/", response_model=schemas.Book)
-def create_book(book: schemas.BookCreate, db: Session = Depends(get_db)):
-    obj = models.Book(title=book.title)
-    db.add(obj)
-    db.commit()
-    db.refresh(obj)
-    return obj
-
 @router.get("/{book_id}", response_model=schemas.Book)
 def get_book(book_id: int, db: Session = Depends(get_db)):
     book = db.query(models.Book).filter(models.Book.id == book_id).first()
@@ -34,8 +44,19 @@ def get_book(book_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Book not found")
     return book
 
+
+
+@router.post("/", response_model=schemas.Book)
+def create_book(book: schemas.BookCreate, db: Session = Depends(get_db), username: str = Depends(get_current_username)):
+    obj = models.Book(title=book.title, author=book.author, description=book.description, year=book.year)
+    db.add(obj)
+    db.commit()
+    db.refresh(obj)
+    return obj
+
+
 @router.delete("/{book_id}")
-def delete_book(book_id: int, db: Session = Depends(get_db)):
+def delete_book(book_id: int, db: Session = Depends(get_db), username: str = Depends(get_current_username)):
     book = db.query(models.Book).filter(models.Book.id == book_id).first()
     if book is None:
         raise HTTPException(status_code=404, detail="Book not found")
@@ -43,17 +64,21 @@ def delete_book(book_id: int, db: Session = Depends(get_db)):
     db.commit()
     return {"message": "Book deleted successfully"}
 
+
 @router.put("/{book_id}", response_model=schemas.Book)
-def update_book(book_id: int, book_update: schemas.BookCreate, db: Session = Depends(get_db)):
+def update_book(book_id: int, book_update: schemas.BookCreate, db: Session = Depends(get_db),
+                username: str = Depends(get_current_username)):
     db_book = db.query(models.Book).filter(models.Book.id == book_id).first()
 
     if db_book is None:
         raise HTTPException(status_code=404, detail="Book not found")
-        
+
     db_book.title = book_update.title
+    db_book.author = book_update.author
+    db_book.description = book_update.description
+    db_book.year = book_update.year
 
     db.commit()
     db.refresh(db_book)
 
     return db_book
-
