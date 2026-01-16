ca0a93a484bce8b0b9a31cec2894a53526d8de9d
Maciej Iskra <s168723@student.uken.krakow.pl>
2026-01-16 16:41:58 +0100

    feat: implement auth endpoints

--- app/schemas.py
+++ app/schemas.py
@@ -17,3 +17,14 @@ class Book(BookBase):
 
     class Config:
         from_attributes = True
+
+class UserCreate(BaseModel):
+    username: str
+    password: str
+
+
+class User(BaseModel):
+    id: int
+    username: str
+    class Config:
+        from_attributes = True
